e598f1487b9ebc4c2bca0325af488b7841b8c4d6
bilalekrem <bilalekremharmansa@gmail.com>
2021-11-06 23:20:39 +0300

    CERT-6 / communicate worker with server over gRPC

--- cmd/cli/cluster_worker.go
+++ cmd/cli/cluster_worker.go
@@ -1,8 +1,13 @@
 package cli
 
 import (
+	"context"
+	"fmt"
+
+	pb "bilalekrem.com/certstore/internal/grpc/proto"
 	"bilalekrem.com/certstore/internal/logging"
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
 )
 
 var clusterWorkerCmd = &cobra.Command{
@@ -30,6 +35,33 @@ var clusterWorkerCreateCertCmd = &cobra.Command{
 	},
 }
 
+var clusterWorkerStartCmd = &cobra.Command{
+	Use:   "communicateServer",
+	Short: "communicat with server",
+	Run: func(cmd *cobra.Command, args []string) {
+		serverAddress, _ := cmd.Flags().GetString("server-address")
+
+		// -----
+
+		var opts []grpc.DialOption
+		opts = append(opts, grpc.WithInsecure())
+
+		conn, err := grpc.Dial(serverAddress, opts...)
+		if err != nil {
+			error("fail to dial gRPC server: %v", err)
+		}
+		defer conn.Close()
+
+		client := pb.NewHelloServiceClient(conn)
+
+		request := &pb.HelloRequest{
+			Name: "certstore!",
+		}
+		response, _ := client.SayHello(context.Background(), request)
+		fmt.Println(response.Message)
+	},
+}
+
 func init() {
 	clusterWorkerCreateCertCmd.Flags().String("address", "", "address of worker")
 	clusterWorkerCreateCertCmd.Flags().String("cacert", "", "certificate authority file path in PEM format")
@@ -38,6 +70,11 @@ func init() {
 	clusterWorkerCreateCertCmd.MarkFlagRequired("cacert")
 	clusterWorkerCreateCertCmd.MarkFlagRequired("cakey")
 
+	// ------
+	clusterWorkerStartCmd.Flags().String("server-address", "", "server address to communicate")
+	clusterWorkerStartCmd.MarkFlagRequired("server-address")
+
 	clusterCmd.AddCommand(clusterWorkerCmd)
 	clusterWorkerCmd.AddCommand(clusterWorkerCreateCertCmd)
+	clusterWorkerCmd.AddCommand(clusterWorkerStartCmd)
 }
