02c766171d1ca093d8b111301f273c97db4873a2
bilalekrem <bilalekremharmansa@gmail.com>
2022-02-08 23:03:44 +0100

    CERT - 28 / new unit tests for worker initialization

--- internal/cluster/worker/worker_test.go
+++ internal/cluster/worker/worker_test.go
@@ -1,14 +1,26 @@
 package worker
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"bilalekrem.com/certstore/internal/assert"
+	"bilalekrem.com/certstore/internal/certificate/service"
+	"bilalekrem.com/certstore/internal/cluster/manager"
 	"bilalekrem.com/certstore/internal/cluster/worker/config"
 	"bilalekrem.com/certstore/internal/pipeline"
 	"bilalekrem.com/certstore/internal/pipeline/action"
 	"bilalekrem.com/certstore/internal/pipeline/store"
 )
+
+func TestNewFromFile(t *testing.T) {
+	testNewFromFile(t, false)
+}
+
+func TestNewFromFileSkipJobInitialization(t *testing.T) {
+	testNewFromFile(t, true)
+}
 
 func TestInitPipelineConfigSuccess(t *testing.T) {
 	worker := &Worker{
@@ -118,5 +130,70 @@ func TestSkipInitializationJobs(t *testing.T) {
 	}
 	err := worker.init(conf, actionStore, true)
 	assert.NotError(t, err, "worker creation failed")
-	assert.Nil(t, worker.jobs)
+	assert.Equal(t, 0, len(worker.jobs))
+}
+
+func testNewFromFile(t *testing.T, skipJobInitialization bool) {
+	dir, err := ioutil.TempDir("/tmp", "test_worker_new_")
+	assert.NotError(t, err, "creating temp dir failed")
+	defer os.RemoveAll(dir)
+
+	// ----
+
+	clusterManager, caCert := createClusterManagerWithClusterCA(t)
+	workerCert, err := clusterManager.CreateWorkerCertificate("worker-cert")
+	assert.NotError(t, err, "certificate could not be created")
+
+	caCertPath := dir + "/ca.crt"
+	err = ioutil.WriteFile(caCertPath, caCert.Certificate, 0666)
+	assert.NotError(t, err, "saving ca failed")
+
+	workerCertPath := dir + "/worker.crt"
+	err = ioutil.WriteFile(workerCertPath, workerCert.Certificate, 0666)
+	assert.NotError(t, err, "saving certificate failed")
+
+	workerCertKeyPath := dir + "/worker.key"
+	err = ioutil.WriteFile(workerCertKeyPath, workerCert.PrivateKey, 0666)
+	assert.NotError(t, err, "saving certificate key failed")
+
+	// -----
+
+	pipelineConfigs := []pipeline.PipelineConfig{
+		{Name: "test-pipeline",
+			Actions: []pipeline.PipelineActionConfig{
+				{Name: "sh"},
+			}},
+	}
+
+	jobConfigs := []config.JobConfig{
+		{Name: "test job", Pipeline: "test-pipeline"},
+	}
+	conf := &config.Config{
+		ServerAddr:       "server-address:server:port",
+		TlsCACert:        caCertPath,
+		TlsWorkerCert:    workerCertPath,
+		TlsWorkerCertKey: workerCertKeyPath,
+		Pipelines:        pipelineConfigs,
+		Jobs:             jobConfigs,
+	}
+
+	worker, err := NewFromConfig(conf, skipJobInitialization)
+	assert.NotError(t, err, "creating worker failed")
+	assert.NotNil(t, worker)
+
+	if skipJobInitialization {
+		assert.Equal(t, 0, len(worker.jobs))
+	}
+}
+
+func createClusterManagerWithClusterCA(t *testing.T) (manager.ClusterManager, *service.NewCertificateResponse) {
+	initialClusterManager, err := manager.NewForInitialization()
+	assert.NotError(t, err, "cluster manager could not be created")
+
+	response, err := initialClusterManager.CreateClusterCACertificate("test-cluster")
+	assert.NotError(t, err, "ca cert could not be created")
+
+	clusterManager, err := manager.NewFromCA(response.Certificate, response.PrivateKey)
+	assert.NotError(t, err, "ca cert could not be created")
+	return clusterManager, response
 }
